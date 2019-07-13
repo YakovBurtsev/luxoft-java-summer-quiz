e98e5414b8ae48003ab7c8f03217a3bac3cd71d2
yakov <yakovburtsev@gmail.com>
2019-07-13 14:13:23 +0300

    Task5

--- src/com/test/Task5.java
+++ src/com/test/Task5.java
@@ -6,7 +6,7 @@ public class Task5 {
 
     public static void main(String[] args) {
 
-        var d = Stream.of(Double.NEGATIVE_INFINITY)
+        var d = Stream.of(Double.NEGATIVE_INFINITY, 1.0, 2.0, 5.0)
                 .parallel()
                 .reduce(10.0, (x, y) -> x / y, (p, q) -> p + q);
 
